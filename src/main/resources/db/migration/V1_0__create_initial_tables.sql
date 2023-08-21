f7f1ca8c3832c880d7ecefe55d4417a92bad787e
eclipse1111 <eclipse.samp@yahoo.com>
2023-08-21 11:45:39 +0300

    Added flyway db migration

--- src/main/resources/db/migration/V1_0__create_initial_tables.sql
+++ src/main/resources/db/migration/V1_0__create_initial_tables.sql
@@ -1,21 +1,19 @@
-CREATE DATABASE [IF NOT EXISTS] budget;
-
-CREATE TABLE [IF NOT EXISTS] account (
-    id bigint NOT NULL,
+CREATE TABLE IF NOT EXISTS account (
+    id SERIAL,
     user_uid uuid NOT NULL,
     currency character varying NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE [IF NOT EXISTS] expense_category (
-    id bigint NOT NULL,
+CREATE TABLE IF NOT EXISTS expense_category (
+    id SERIAL,
     name character varying NOT NULL,
     uid uuid NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE [IF NOT EXISTS] expense (
-    id bigint NOT NULL,
+CREATE TABLE IF NOT EXISTS expense (
+    id SERIAL,
     amount double precision NOT NULL,
     name character varying NOT NULL,
     description character varying NOT NULL,
@@ -32,15 +30,15 @@ CREATE TABLE [IF NOT EXISTS] expense (
             REFERENCES expense_category(id)
 );
 
-CREATE TABLE [IF NOT EXISTS] income_category (
-    id bigint NOT NULL,
+CREATE TABLE IF NOT EXISTS income_category (
+    id SERIAL,
     name character varying NOT NULL,
     uid uuid NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE [IF NOT EXISTS] income (
-    id bigint NOT NULL,
+CREATE TABLE IF NOT EXISTS income (
+    id SERIAL,
     amount double precision NOT NULL,
     description character varying NOT NULL,
     account_id bigint NOT NULL,
@@ -55,3 +53,13 @@ CREATE TABLE [IF NOT EXISTS] income (
         FOREIGN KEY (category_id)
             REFERENCES income_category(id)
 );
+
+
+
+INSERT INTO income_category(id, name, uid) VALUES (DEFAULT, 'cat1', '74a8901c-3755-11ee-be56-0242ac120002');
+INSERT INTO income_category(id, name, uid) VALUES (DEFAULT, 'cat2', '848124cc-3755-11ee-be56-0242ac120002');
+INSERT INTO income_category(id, name, uid) VALUES (DEFAULT, 'cat3', '8ab873ae-3755-11ee-be56-0242ac120002');
+
+INSERT INTO expense_category(id, name, uid) VALUES (DEFAULT, 'cat1', '74a8901c-3755-11ee-be56-0242ac120002');
+INSERT INTO expense_category(id, name, uid) VALUES (DEFAULT, 'cat2', '848124cc-3755-11ee-be56-0242ac120002');
+INSERT INTO expense_category(id, name, uid) VALUES (DEFAULT, 'cat3', '8ab873ae-3755-11ee-be56-0242ac120002');
