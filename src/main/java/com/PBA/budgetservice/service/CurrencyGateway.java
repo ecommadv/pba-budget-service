9311fc793eeb3085161ec3fd5e75671115db278f
eclipse1111 <eclipse.samp@yahoo.com>
2023-09-02 16:10:18 +0300

    Solved the comments

--- src/main/java/com/PBA/budgetservice/service/CurrencyGateway.java
+++ src/main/java/com/PBA/budgetservice/service/CurrencyGateway.java
@@ -3,6 +3,6 @@ package com.PBA.budgetservice.service;
 import java.math.BigDecimal;
 import java.util.Map;
 
-public interface CurrencyService {
+public interface CurrencyGateway {
     public Map<String, BigDecimal> getCurrencyExchangeRatesMapping();
 }
---
+++ src/main/java/com/PBA/budgetservice/utils/XmlChildElementExtractor.java
@@ -0,0 +1,7 @@
+package com.PBA.budgetservice.utils;
+
+import org.jdom2.Element;
+
+public interface XmlChildElementExtractor {
+    public Element getChildOfElement(Element rootNode);
+}
